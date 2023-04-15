8bd0fc0d20addd76d02ffe1ecab9ddd5acb95607
Mahdi <51413761+metttirezzi@users.noreply.github.com>
2023-04-15 14:15:27 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req , res ) => {
+    res.send('Hello World');
+});
+
+app.get('/api/courses' , (req , res) => {
+    res.send([1,2,3]);
+});
+
+//Environment Variable 
+//PORT
+const port = process.env.PORT || 3000;
+
+app.listen(port , () => console.log(`lisetning on port ${port}...`));
